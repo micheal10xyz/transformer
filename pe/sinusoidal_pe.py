4ac86baa68170ce28086df456eae523d6221422b
michael10 <qiningshi@gmail.com>
2025-07-27 19:31:16 +0800

    添加正余弦编码

--- pe/sinusoidal_pe.py
+++ pe/sinusoidal_pe.py
@@ -15,7 +15,7 @@ class SinusoidalPE(nn.Module):
         self.pe = torch.zeros(max_len, dim)
         pos = torch.arange(0, max_len, dtype=torch.float16).unsqueeze(1)
         base = 10000
-        base_pow = torch.pow(base, torch.arange(0, max_len, 2, dtype=torch.float16) / dim).unsqueeze(0)
+        base_pow = torch.pow(base, torch.arange(0, dim, 2, dtype=torch.float16) / dim).unsqueeze(0)
         self.pe[::, 0::2] = torch.sin(pos / base_pow)
         self.pe[::, 1::2] = torch.cos(pos / base_pow)
     
@@ -24,26 +24,41 @@ class SinusoidalPE(nn.Module):
         Args:
             X (tensor): 小批量词嵌入张量，shape为(b, n, dim)， b为小批量大小，n为token的数量，dim为token embedding的维度
         """
-        return X + torch.unsqueeze(self.pe[0:X.shape[1], ::])
+        return X + torch.unsqueeze(self.pe[0:X.shape[1], ::], dim=0)
     
 
-def num_to_binary(num, bits):
-    bins = []
-    bit = 1
-    for i in range(bits):
-        bins.append(1 if num &bit > 0 else 0)
-        bit = bit << 1
-    return bits
 
 
-def num_bits_tensor(num, bits):
-    return torch.tensor([num_to_binary(i, bits) for i in range(num)])
+# pe = SinusoidalPE(256, 50)
+# plt.figure(figsize=(10,6))
+# plt.imshow(pe.pe.numpy(), cmap='viridis', aspect='auto')
+# plt.xlabel('Encoding Dimension')
+# plt.ylabel('Token Position')
+# plt.colorbar()
+# plt.title('Positional Encoding Heatmap')
+# plt.show()
 
 
-plt.figure(figsize=(10,6))
-plt.imshow(pe.numpy(), cmap='viridis', aspect='auto')
-plt.xlabel('Encoding Dimension')
-plt.ylabel('Token Position')
-plt.colorbar()
-plt.title('Positional Encoding Heatmap')
-plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+# def num_to_binary(num, bits):
+#     bins = []
+#     bit = 1
+#     for i in range(bits):
+#         bins.append(1 if num &bit > 0 else 0)
+#         bit = bit << 1
+#     return bits
+
+
+# def num_bits_tensor(num, bits):
+#     return torch.tensor([num_to_binary(i, bits) for i in range(num)])
